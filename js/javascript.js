2606630677f63bd6b65b6fcef6e3c3a503d92694
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-06-02 02:30:12 -0400

    Add operate function, test switch on operator argument, pass tests

--- js/javascript.js
+++ js/javascript.js
@@ -13,3 +13,20 @@ const multiply = function (a, b) {
 const divide = function (a, b) {
   return a / b;
 };
+
+const operate = function (operator) {
+  switch (operator) {
+    case "+":
+      console.log("Add");
+      break;
+    case "-":
+      console.log("Subtract");
+      break;
+    case "*":
+      console.log("Multiply");
+      break;
+    case "/":
+      console.log("Divide");
+      break;
+  }
+};
